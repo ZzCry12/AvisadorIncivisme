035b320f069c02f1b52c4f0cd91913e4a2fe51a7
ZzCry12 <dariorata@gmail.com>
2022-11-15 14:51:31 +0100

    v1.0

--- app/src/main/java/com/example/avisadorincivisme/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/avisadorincivisme/ui/home/HomeFragment.java
@@ -1,31 +1,51 @@
 package com.example.avisadorincivisme.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
+
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.avisadorincivisme.SharedViewModel;
 import com.example.avisadorincivisme.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
 
+
+public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        SharedViewModel sharedViewModel = new ViewModelProvider(getActivity()).get(SharedViewModel.class);
+
+        SharedViewModel.getCurrentAddress().observe(getViewLifecycleOwner(), address -> {
+            binding.localitzacio.setText(String.format(
+                   // "Direcció: %1$s \n Hora: %2$tr",
+                    address, System.currentTimeMillis()));
+        });
+        sharedViewModel.getButtonText().observe(getViewLifecycleOwner(), s -> binding.buttonlocation.setText(s));
+        sharedViewModel.getProgressBar().observe(getViewLifecycleOwner(), visible -> {
+            if (visible)
+                binding.loading.setVisibility(ProgressBar.VISIBLE);
+            else
+                binding.loading.setVisibility(ProgressBar.INVISIBLE);
+        });
+
+        binding.buttonlocation.setOnClickListener(view -> {
+            Log.d("DEBUG", "Clicked Get Location");
+            sharedViewModel.switchTrackingLocation();
+        });
+
         return root;
     }
 
--- app/src/main/java/com/example/avisadorincivisme/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/avisadorincivisme/ui/notifications/NotificationsFragment.java
@@ -34,4 +34,5 @@ public class NotificationsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
